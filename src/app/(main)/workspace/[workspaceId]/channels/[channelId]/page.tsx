c304d654c584eceb90f6bff7c235edf84fae7946
Chinmay-innovates <chinmaygope69@gmail.com>
2025-06-17 18:29:20 +0530

    Editor content

--- src/app/(main)/workspace/[workspaceId]/channels/[channelId]/page.tsx
+++ src/app/(main)/workspace/[workspaceId]/channels/[channelId]/page.tsx
@@ -1,12 +1,14 @@
 import { redirect } from 'next/navigation';
 
-import { InfoSection } from '@/components/info-section';
-import { Sidebar } from '@/components/sidebar';
 import { getUserData } from '@/server/get-user-data';
-import { Workspace as WorkspaceType } from '@/types/app';
 import { getCurrentWorkspaceData, getUserWorkspaceData } from '@/server/workspaces';
 import { getUserWorkspaceChannels } from '@/server/get-user-workspace-channels';
 import { Typography } from '@/components/ui/typography';
+import { ChatHeader } from '@/components/chat-header';
+import { TextEditor } from '@/components/text-editor';
+import { InfoSection } from '@/components/info-section';
+import { Sidebar } from '@/components/sidebar';
+import { Workspace as WorkspaceType } from '@/types/app';
 
 const Channel = async ({
   params,
@@ -28,21 +30,39 @@ const Channel = async ({
 
   const userWorkspaceChannels = await getUserWorkspaceChannels(currentWorkspaceData.id, user.id);
 
+  const channel = userWorkspaceChannels.find((channel) => channel.id === params.channelId);
+  if (!channel) return redirect('/');
+
   return (
     <div className="hidden md:block">
-      <Sidebar
-        currentWorkspaceData={currentWorkspaceData}
-        userData={user}
-        userWorkspacesData={userWorkspacesData as WorkspaceType[]}
-      />
-      <InfoSection
-        currentWorkspaceData={currentWorkspaceData}
-        userData={user}
-        userWorkspaceChannels={userWorkspaceChannels}
-        currentChannelId={params.channelId}
-      />
-      <div className="p-2">
-        <Typography text="Channel" variant="h3" />
+      <div className="h-[calc(100vh-256px)] overflow-y-auto [&::-webkit-scrollbar-thumb]:rounded-[6px] [&::-webkit-scrollbar-thumb]:bg-foreground/60 [&::-webkit-scrollbar-track]:bg-none [&::-webkit-scrollbar]:w-2">
+        <Sidebar
+          currentWorkspaceData={currentWorkspaceData}
+          userData={user}
+          userWorkspacesData={userWorkspacesData as WorkspaceType[]}
+        />
+        <InfoSection
+          currentWorkspaceData={currentWorkspaceData}
+          userData={user}
+          userWorkspaceChannels={userWorkspaceChannels}
+          currentChannelId={params.channelId}
+        />
+        <div className="p-4 relative w-full overflow-hidden">
+          <ChatHeader title={channel.name} />
+          <div className="mt-10">
+            <Typography text="Chat Content" variant="h3" />
+          </div>
+        </div>
+      </div>
+
+      <div className="m-4">
+        <TextEditor
+          apiUrl={''}
+          type={'Channel'}
+          workspaceData={currentWorkspaceData}
+          userData={user}
+          channel={channel}
+        />
       </div>
     </div>
   );
---
+++ src/components/chat-header.tsx
@@ -0,0 +1,17 @@
+import { IoMdHeadset } from 'react-icons/io';
+
+import { Typography } from './ui/typography';
+
+type Props = {
+  title: string;
+};
+export const ChatHeader = ({ title }: Props) => {
+  return (
+    <div className="absolute z-20 h-10 top-0 left-0 w-full">
+      <div className="h-10 flex items-center justify-between px-4 fixed md:w-[calc(100%-305px)] lg:w-[calc(100%-447px)] bg-white dark:bg-neutral-800 border-b border-b-white/30 shadow-md">
+        <Typography text={`# ${title}`} variant="h4" />
+        <IoMdHeadset className="text-primary cursor-pointer" size={24} />
+      </div>
+    </div>
+  );
+};
